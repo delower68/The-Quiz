a00988581261fdbce622ea61853bed71561a0080
delower68 <hdelower68@gmail.com>
2022-10-11 00:29:41 +0800

    navbar set

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import ErrorPage from './components/ErrorPage/ErrorPage';
+import Main from './components/Main/Main';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path:'/',
+      element: <Main/>,
+      errorElement: <ErrorPage/> ,
+      children:[
+        {
+
+        }
+      ]
+    }
+  ])
   return (
-    <div className="App">
-      
-    </div>
+    <RouterProvider router={router}/>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div>
+      <header className="p-4 dark:bg-gray-800 dark:text-gray-100">
+        <div className="container flex justify-between h-16 mx-auto">
+          <h1 className="flex items-center font-bold text-3xl">The Quiz</h1>
+          <ul className="items-stretch hidden space-x-3 md:flex">
+            <li className="flex">
+              <Link
+                
+                to="#"
+                className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent"
+              >
+                Home
+              </Link>
+            </li>
+            <li className="flex">
+              <Link
+                
+                to="#"
+                className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent"
+              >
+                Topics
+              </Link>
+            </li>
+            <li className="flex">
+              <Link
+                
+                to="#"
+                className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent"
+              >
+                Statistic
+              </Link>
+            </li>
+            <li className="flex">
+              <Link
+                
+                to="#"
+                className="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent"
+              >
+                Blog
+              </Link>
+            </li>
+          </ul>
+          <button className="flex justify-end p-4 md:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
